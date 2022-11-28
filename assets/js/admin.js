06db19f25f83dd15e52fe620e3fda75bb7184931
facina <lovemistborn13@gmail.com>
2022-11-29 00:16:44 +0800

    Update 2022-11-28T16:16:40.743Z

---
+++ assets/js/admin.js
@@ -0,0 +1,198 @@
+const api_Path = '2022nov';
+const api_Src = "https://livejs-api.hexschool.io/api/livejs/v1";
+const token = "BvRBbqadN2RAHi0hwsvYiN0VQAy1";
+const js0rderList = document.querySelector('.js-orderList');
+
+let orderData = [];
+
+
+getOrderList();
+
+// axios get訂單列表+順便執行c3
+function getOrderList(){
+    axios.get(`${api_Src}/admin/${api_Path}/orders`,{
+        headers:{
+           "Authorization": token
+        }
+    })
+  .then(function(response){
+    orderData = response.data.orders;
+    renderOrderList(orderData);
+    renderc3();
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+
+//渲染c3圖表
+function renderc3(){
+    let total = {};
+    orderData.forEach(item =>{
+    const {products} = item;
+    products.forEach(el =>{
+    if(total[el.category]==undefined){
+     total[el.category]=el.price*el.quantity;  
+    }else{
+     total[el.category]+=el.price*el.quantity;  
+    }
+    })
+    })
+    let categoryArr = Object.keys(total);
+    let newData = [];
+    categoryArr.forEach(el =>{
+     let arr = [];
+     arr.push(el);
+     arr.push(total[el]);
+     newData.push(arr);
+    })
+    newData.sort(function(a,b){
+        return b[1]-a[1];
+    })
+    let otherTotal = 0;
+    if(newData.length > 3){   
+      newData.forEach((el,i) =>{
+        if(i>2){
+            otherTotal+=el[1];
+        }
+      })
+    }
+    newData.splice(3,newData.length-1)
+    newData.push(['其他',otherTotal])
+
+
+    let chart = c3.generate({
+        bindto: '#chart', 
+        data: {
+            type: "pie",
+            columns: newData,
+            colors:{
+                "窗簾":"#DACBFF",
+                "床架":"#9D7FEA",
+                "收納": "#5434A7",
+                "其他": "#301E5F",
+            }
+        },
+    });
+}
+
+ 
+
+
+
+// 渲染訂單order list
+function renderOrderList(data){
+    let str ="";
+    data.forEach(el =>{
+        const {user, id, createdAt, paid, products} = el;
+        //組成時間
+        const timeStamp = new Date(createdAt*1000);
+        const time = `${timeStamp.getFullYear()}/${timeStamp.getMonth() + 1}/${timeStamp.getDate()}`;
+        let productStr ="";
+        products.forEach(item =>{
+        productStr +=`<p>${item.title}x${item.quantity}</p>`;
+        })
+        let orderStatus ="";
+        if(paid==true){
+            orderStatus ="已處理";
+        }else{
+            orderStatus ="未付款";
+        }
+        str+= `<tr>
+        <td width="10%">${id}</td>
+        <td width="10%">
+          <p>${user.name}</p>
+          <p>${user.tel}</p>
+        </td>
+        <td width="10%">${user.address}</td>
+        <td width="15%">${user.email}</td>
+        <td width="25%">
+        ${productStr}
+        </td>
+        <td width="10%">${time}</td>
+        <td width="10%" class="orderStatus">
+          <a href="#" data-id="${id}" data-status="${paid}" class="orderStatus">${orderStatus}</a>
+        </td>
+        <td width="10%">
+          <input type="button" class="delSingleOrder-Btn" data-id="${id}" value="刪除">
+        </td>
+    </tr>`;
+  
+    js0rderList.innerHTML = str; //放在forEach裡才渲染得出來
+    }) 
+   
+}
+
+js0rderList.addEventListener('click',function(e){
+    e.preventDefault();
+    const targetClass = e.target.getAttribute('class'); //抓到未付款＆刪除的class
+    const itemId = e.target.getAttribute('data-id'); //兩個id一樣，class不同去二分情況
+    const paidStatus = e.target.getAttribute('data-status');
+    if(targetClass =="orderStatus"){
+        console.log(itemId);
+        console.log(paidStatus);
+        changeStatus(paidStatus,itemId);
+        return;
+    }else if(targetClass =="delSingleOrder-Btn"){
+     deleteOrder(itemId);
+    }
+})
+
+//寫個別『刪除訂單資料』的函式axios delete
+
+function deleteOrder(id){
+  axios.delete(`${api_Src}/admin/${api_Path}/orders/${id}`,{
+    headers:{
+       "Authorization": token
+    }
+})
+  .then(function(response){
+    getOrderList();
+  })
+}
+
+//寫個別『更改訂單狀態』的函式axios put -注意axios規定參數格式
+
+function changeStatus(status,id){
+    console.log(status);
+    let updateStatus;
+    if(status=="true"){
+        updateStatus=false;
+        console.log(`現在改成${updateStatus}`)
+    }else{
+        updateStatus=true;
+        console.log(`現在改成${updateStatus}`)
+    }
+    axios.put(`${api_Src}/admin/${api_Path}/orders`,{
+        "data": {
+          "id": id,
+          "paid": updateStatus
+        }
+      },{
+        headers:{
+           "Authorization": token
+        }
+    })
+      .then(function(response){
+        getOrderList();
+        console.log('修改訂單狀態成功！');
+      })
+      .catch(function(error){
+        console.log(error);
+      })
+}
+
+//utility js
+//千分位
+ 
+function toThousands(x) {
+     let parts = x.toString().split("."); 
+     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
+     return parts.join("."); 
+}
+
+
+
+
+
---
+++ assets/js/index.js
@@ -0,0 +1,375 @@
+
+const productWrap = document.querySelector('.productWrap');
+const productSelect = document.querySelector('.productSelect');
+const api_Url = "https://livejs-api.hexschool.io/api/livejs/v1/customer/2022nov";
+const api_Urlm = "https://livejs-api.hexschool.io/api/livejs/v1/admin/2022nov";
+const api_Path = '2022nov';
+const api_Src = "https://livejs-api.hexschool.io/api/livejs/v1";
+const shoppingCartTable = document.querySelector('.shoppingCart-table');
+const cardItemTitle = document.querySelector('.cardItem-title');
+const perItemCart = document.querySelector('.perItemCart');
+const jsTotal = document.querySelector('.js-total');
+const orderInfoBtn = document.querySelector('.orderInfo-btn');
+const token = "BvRBbqadN2RAHi0hwsvYiN0VQAy1";
+
+//axios.get資料
+
+let wData = [];
+
+axiosGet();
+    getCart();
+    productSelect.value = "全部";
+init();
+
+//get一般product
+function axiosGet(){
+  axios.get(`${api_Src}/customer/${api_Path}/products`)
+  .then(function(response){
+    wData = response.data.products;
+    console.log(wData);
+    console.log("成功");
+    renderData(wData,productWrap);  
+})
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+//渲染一般product
+function renderData(data,domElement){
+    let str ="";
+    data.forEach(item =>{
+        if(data.length>0){
+        str += productHTML(item) }
+    else{
+        return "";
+    }
+    domElement.innerHTML = str;
+    })  
+    
+}
+
+//篩選一般product
+
+productSelect.addEventListener('change', e =>{
+    let categorySelect = e.target.value;
+    const filterData = wData.filter(el =>{
+        return el.category === categorySelect;
+    })
+    categorySelect === "全部"?renderData(wData):renderData(filterData);
+})
+
+//初始化
+function init(){
+    axiosGet();
+    getCart();
+    productSelect.value = "全部";
+}
+
+let subtotal = 0;
+//函式消除重複
+function cartHTML(item){
+    const {title,images,price} = item.product;
+    const {quantity, id} = item;
+    subtotal = price*quantity;
+    return `<tr class="perItem"><td>
+    <div class="cardItem-title">
+        <img src="${images}" alt="">
+        <p>${title}</p>
+    </div>
+</td>
+<td>NT$${toThousands(price)}</td>
+<td>${quantity}</td>
+<td class="subtotal">NT$${toThousands(subtotal)}</td>
+<td class="discardBtn">
+<a href="#" class="material-icons" data-id="${id}"> 
+    clear
+</a>
+</td></tr>`
+} //注意取用cart的id還是product的id
+
+function productHTML(item){
+    const {id, images, title,origin_price, price} = item;
+    return ` <li class="productCard">
+    <h4 class="productType">新品</h4>
+    <img src="${images}" alt="">
+    <a href="#" class="addCardBtn" data-id="${id}">加入購物車</a>
+    <h3>${title}</h3>
+    <del class="originPrice">NT$${toThousands(origin_price)}</del>
+    <p class="nowPrice">NT$${toThousands(price)}</p>
+    </li> `
+}
+
+//get購物車carts
+
+let cData = [];
+
+function getCart(){
+    axios.get(`${api_Src}/customer/${api_Path}/carts`)
+  .then(function(response){
+    cData = response.data.carts;
+    let finalTot = response.data.finalTotal;
+    rendercData(cData,perItemCart)
+    jsTotal.textContent = toThousands(finalTot);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+
+//渲染購物車 carts
+function rendercData(data,domElement){
+    let str ="";
+    data.forEach(item =>{
+    if(data.length>0){
+         str += cartHTML(item);  
+ }
+})   
+    domElement.innerHTML = str;
+
+    console.log('渲染購物車成功');
+}
+
+//『加入購物車』按鈕的監聽//技巧：監聽整個顯示商品的ul區域，可以限制監聽行為不會莫名其妙跑到其他區塊去，一個一個綁效能很差。缺點二：萬一寫到後面有一樣的li的class名稱，監聽會套用在你沒預期的地方。
+
+productWrap.addEventListener('click', (e) => {
+    e.preventDefault();
+    let addBtnClass = e.target.getAttribute('class');
+    if (addBtnClass !== 'addCardBtn') {
+        return
+    }
+    let productId = e.target.getAttribute('data-id');
+    let num = 1;
+    cData.forEach(item =>{
+        if(item.product.id===productId){
+            num = item.quantity +=1;
+        }else{
+            item.quantity = num;
+        }
+    })
+    console.log(num);
+    axios.post(`${api_Url}/carts`,{
+        "data": {
+            "productId": productId,
+            "quantity": num
+          }
+    }).then(function(response){
+        alert('加入購物車成功！')
+        getCart();
+    })
+
+})
+//『刪除單筆』按鈕的監聽//多個按鈕，往上找父層，僅綁tbody
+
+perItemCart.addEventListener('click', (e) =>{
+    e.preventDefault();
+    const cartId = e.target.getAttribute('data-id');
+    console.log(cartId);
+    if(cartId==null){
+        return;
+    }else{
+        axios.delete(`${api_Url}/carts/${cartId}`)
+        .then(function(response){
+            console.log(response);
+            alert('刪除購物車成功！')   
+            getCart();    
+        })  
+    }
+   
+})
+
+const discardAllBtn = document.querySelector('.discardAllBtn');
+
+//『刪除全部』按鈕的監聽//單個按鈕，僅綁單個按鈕
+
+discardAllBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    axios.delete(`${api_Url}/carts/`)
+        .then(function(response){
+            alert('購物車成功清空！')   
+            getCart();    
+        })
+        .catch(function(err){
+            alert('已清空，勿重複點擊');
+        })  
+    })
+
+//『送出預定資料』按鈕的監聽//單個按鈕，僅綁單個按鈕
+orderInfoBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    if(cData.length==0){
+    alert("購物車裡尚未有品項，請加入至少一項再送出！");
+    return;
+    }
+    let customerName = document.querySelector('#customerName').value;
+    let customerPhone = document.querySelector('#customerPhone').value;
+    let customerEmail = document.querySelector('#customerEmail').value;
+    let customerAddress = document.querySelector('#customerAddress').value;
+    let tradeWay = document.querySelector('#tradeWay').value;
+    if(customerName==""||customerPhone==""|| customerAddress==""|| customerEmail==""){
+        alert("資料不全，請檢查再送出！");
+        return;
+    }
+    axios.post(`${api_Url}/orders`,{
+        "data": {
+          "user": {
+            "name": customerName,
+            "tel": customerPhone,
+            "email": customerEmail,
+            "address": customerAddress,
+            "payment": tradeWay
+          }
+        }
+      }).then(function(response){
+        alert('送出訂單成功！')
+        console.log(response.data);
+        getOrder();
+        getCart();
+        customerName = "";
+        customerPhone = "";
+        customerEmail = "";
+        customerAddress = "";
+        tradeWay= "";
+    })
+}) 
+
+//get購物車carts
+
+
+function getOrder(){
+    axios.get(`${api_Urlm}/orders`,{
+        headers:{
+           "Authorization": token
+        }
+    })
+  .then(function(response){
+    console.log(response.data);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+//validate js
+
+const inputs = document.querySelectorAll('input[name],select[data=payment]')
+const orderInfoForm = document.querySelector('.orderInfo-form')
+
+orderInfoBtn.addEventListener('click', e =>{
+    //validate js 套件的規則
+    const constraints = {
+        "姓名": {
+            presence: {
+                message: "必填欄位"
+            }
+        },
+        "手機號碼": {
+            presence: {
+                message: "是必填的欄位"
+            },
+            format: {
+                pattern: /^09\d{2}-?\d{3}-?\d{3}$/,
+                message: "開頭須為09"
+            },
+            length: {
+                is: 10,
+                message: "長度須為10碼"
+            }
+        },
+        "信箱": {
+            presence: {
+                message: "是必填的欄位"
+            },
+            format: {
+                pattern: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
+                message: "格式輸入錯誤，需有@ 、.等符號"
+            },
+        },
+        "寄送地址": {
+            presence: {
+                message: "必填欄位"
+            }
+        }
+    };
+    inputs.forEach(el =>{
+        el.addEventListener('blur', function(){
+            el.nextElementSibling.textContent = ''; //這邊的nextElementSibling是警示文字的元素
+            let errors = validate(orderInfoForm, constraints) || '';
+            console.log(errors);
+            console.log("here");
+            Object.keys(errors).forEach(function(el){
+                document.querySelector(`[data-message="${el}"]`).textContent = errors[el];
+                })
+        })
+    })
+    let errors = validate(orderInfoForm, constraints);
+    if(errors){
+        console.log(errors);
+
+    }else{
+        alert("表單成功送出！");
+        form.reset();
+    }
+
+})
+
+
+/**/
+/*async function clearAfterOrder(){
+    await  axios.post(`${api_Url}/orders`,{
+        "data": {
+          "user": {
+            "name": customerName,
+            "tel": customerPhone,
+            "email": customerEmail,
+            "address": customerAddress,
+            "payment": tradeWay
+          }
+        }
+      }).then(function(response){
+        alert('送出訂單成功！')
+        customerName = "";
+        customerPhone = "";
+        customerEmail = "";
+        customerAddress = "";
+        tradeWay= "";})
+
+    await  axios.delete(`${api_Url}/carts/`)
+      .then(function(response){
+          getCart();
+      })
+}*/
+
+  /*|| 
+  addCardBtn.forEach((btn) =>{
+        btn.addEventListener('click', (e) => {
+        const idVal = e.target.getAttribute('id');
+        let num = 1;
+        //舊盲點：一直想要使用畫面上已有的元素，但沒想到依賴api的部分
+        cData.forEach((el) =>{
+            if(el.product.id===idVal){
+                console.log("下方車已有");
+                num = item.quantity +=1;
+            }else{
+            console.log("下方車未有");
+             let print = document.createElement('tr');
+             let lastCart = shoppingCartTable.lastChild;
+             cData.filter(item =>{
+                item.product.id === idVal;
+                print.innerHTML = cartHTML(item); 
+        print.setAttribute('id',idVal);
+        shoppingCartTable.insertBefore(print,lastCart);//效能（？
+             })
+            
+            }
+        })
+    })
+  })*/
+
+
+
+
+
+
+
+
